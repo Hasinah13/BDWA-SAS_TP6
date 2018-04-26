7f13eb89b47790fdd8db082773ec77bf957547be
Joao Mendes <joao.mndsdc@gmail.com>
2018-04-26 16:32:31 +0200

    Point 3 started - Loop tested

--- batman.sh
+++ batman.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
-numArgs=$#
-lastArg="${!numArgs}"
+Args=$1
 
 # check if last argument is directory
 
-if [[ -d "$lastArg" ]]; then
+if [[ -d "$Args" ]]; then
    echo "it is a directory"
 else
    echo "it is not a directory"
 fi
+
+# if it is not a directory
+
+while [ -d ${!Args} ]
+do
+	echo "Bad path"
+done
+
+echo "Good path"
