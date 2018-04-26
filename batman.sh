65ecc6cbc42b624c6fd40b54e27189dcbadcceec
Maraen <31545382+Maraen@users.noreply.github.com>
2018-04-26 16:17:45 +0200

    checking for valid path

--- batman.sh
+++ batman.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+
+numArgs=$#
+lastArg="${!numArgs}"
+
+# check if last argument is directory
+
+if [[ -d "$lastArg" ]]; then
+   echo "it is a directory"
+else
+   echo "it is not a directory"
+fi
