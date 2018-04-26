48d8040b2c62580ae55d98d809347959553adcc5
Victor Audétat <victor@MBP-de-Victor.crea.loc>
2018-04-26 16:08:17 +0200

    test chemin

--- spacecat.sh
+++ spacecat.sh
@@ -1,3 +1,9 @@
 #demande chemin d'un dossier en premier argumente
 echo -n "Quel est le chemin du dossier ?"
 read chemin_entre
+
+if [ -d $chemin_entre = 0 ]; then
+	echo -n "whou"
+else 
+	echo -n "3"
+fi
