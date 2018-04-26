ae200cb1c9999be6843a3d12226bf684f57fe674
Karen Tschanz <karen.tschanz@crea-inseec.com>
2018-04-26 16:30:03 +0200

    argument 1 ok et test dossier ok

--- spacecat.sh
+++ spacecat.sh
@@ -1,9 +1,12 @@
+#!/bin/bash
+
 #demande chemin d'un dossier en premier argumente
 echo -n "Quel est le chemin du dossier ?"
-read chemin_entre
+read arr1[1]
 
-if [ -d $chemin_entre = 0 ]; then
-	echo -n "whou"
-else 
-	echo -n "3"
+if [ -d ${arr1[1]} ] ; then
+    echo "c'est un dossier"
+else
+    echo "ce n'est pas un dossier"
 fi
+
