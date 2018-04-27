d10c9f3c3c5a48afa5dcc2d2685eeb2c98db12af
Maraen <31545382+Maraen@users.noreply.github.com>
2018-04-27 08:35:42 +0200

    Added a do while loop to check if the re entered value is a directory or not

--- batman.sh
+++ batman.sh
@@ -1,12 +1,30 @@
 #!/bin/bash
 
-numArgs=$#
-lastArg="${!numArgs}"
+
+Args=$1
+
 
 # check if last argument is directory
 
-if [[ -d "$lastArg" ]]; then
+if [[ -d "$Args" ]]; then
    echo "it is a directory"
 else
    echo "it is not a directory"
+   echo -n "Please enter a valid argument :"
+   read text
+   echo "$text"
+   while [[ ! -d "$text" ]]; 
+   do
+   	echo -n "this is not a valid directory please check again "
+   	read text
+   	echo "$text"
+   	#close to the correct answer, fixed infinite loop, try something with case $var in -d
+   	#for archive right something as 
+   	#echo var name:
+   	#read varname
+   	#echo file >> /tmp/archive/$varname
+   done
+
+echo "gg"
+
 fi
